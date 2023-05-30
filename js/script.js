046926c862febbf6a816dee79a24ec69e96fa75e
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-05-30 18:13:27 +0200

    js reset

--- js/script.js
+++ js/script.js
@@ -1,39 +1,9 @@
 //JS RESET
 console.log('JS OK')
-/*
-//---Calcolatore:
 
-//Elemento DOM
-const totalElement = document.getElementById('total');
-
-//Messaggio per l'utente
-const message = 'Il prezzo del tuo biglietto è: € ';
-
-//Prezzo in base ai KM
-const priceKm = 0.21;
-
-//Discount
-let discount = null;
-
-//Info utente
-const kms = parseInt(prompt('Quanti chilomentri devi percorrere?', 100).trim());
-const age = parseInt(prompt('Quanti anni hai?', 18).trim()); 
-
-//Prezzo calcolator
-const basePrice = priceKm * kms;
-let finalPrice = basePrice
-
-if(age >= 65){
-    discount = 40;
-} else if (age <= 18){
-    discount = 20;
-}
-
-//Inserisco Price nell'elemento
-totalElement.innerText = message + finalPrice.toFixed(2);
-*/
 
 /*
+----- MIA SCALETTA (sbagliata) -----
  1- Prendiamo gli input dal DOM tramite gli ID
  2- Tramite ELEMENT.VALUE andiamo a prendere i valori inseriti dall'utente
  3- Tramite (button.addEventListener ecc..) salvo i valori inseriti dall'utente
@@ -41,29 +11,31 @@ totalElement.innerText = message + finalPrice.toFixed(2);
  5- Stampo il risultato dei calcoli nella parte sottostante al form
 */
 
-//Prendiamo gli input dal DOM tramite gli ID
-const input = document.querySelector("input");
 /*
-const userName = document.querySelector("#user-name");
-const surname = document.querySelector("#surname");
-const km = document.querySelector("#km");
-const age = document.querySelector("#ag");
+----- SCALETTA di Marco -----
+Scrivere un programma che chieda all’utente:
+
+- Il proprio nome
+- Il numero di chilometri da percorrere
+- Età del passeggero
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, 
+secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+
+# MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due 
+input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.   
+La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+# MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form 
+(non tag <form>) in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina 
+(il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 */
-const btn = document.querySelector("button");
-const ticket = document.querySelector("#ticket");
 
 
-//Tramite ELEMENT.VALUE andiamo a prendere i valori inseriti dall'utente
 
-
-//Tramite (button.addEventListener ecc..) salvo i valori inseriti dall'utente
-btn.addEventListener ('click', function () {
-   const userName = input.value;
-   const surname = input.value;
-   const km = input.value;
-   const age = input.value;
-
-}) ;
-
-
-console.log(btn);
